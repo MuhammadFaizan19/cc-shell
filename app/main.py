7cc53d23a4f428a658cd4f3c8eb3237f2cc11ca8
muhammadFaizan <faizanvkey@gmail.com>
2025-02-07 23:37:51 +0500

    add repl: read-eval-print-loop

--- app/main.py
+++ app/main.py
@@ -4,8 +4,17 @@ import sys
 def main():
     sys.stdout.write("$ ")
 
-    command = input()
+    while True:
+        command = input()
 
+        if command.startswith('exit'):
+            break
+
+        execute_command(command)
+
+        sys.stdout.write("$ ")
+
+def execute_command(command):
     print(f'{command}: command not found')
 
 if __name__ == "__main__":
